2455554e20f8bd66e843dce0f90d8d4e787f3531
Omar <orodriguez19@cmc.edu>
2016-07-09 18:48:12 -0400

    BAD STUFF

--- hackathon/routes/index.js
+++ hackathon/routes/index.js
@@ -2,28 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 // soundcloud authentification
-var SC = require('node-soundcloud');
-
-// Initialize client
-SC.init({
-  id: process.env,
-  secret: 'universe',
-  uri: 'http://localhost:3000/'
-});
-
-// Connect user to authorize application
-var initOAuth = function(req, res) {
-  var url = SC.getConnectUrl();
-
-  res.writeHead(301, Location: url);
-  res.end();
-};
+// var SC = require('soundcloud');
+//
+// // Initialize client
+// SC.init({
+//   id: process.env,
+//   secret: 'universe',
+//   uri: 'http://localhost:3000/'
+// });
+//
+// // Connect user to authorize application
+// var initOAuth = function(req, res) {
+//   var url = SC.getConnectUrl();
+//
+//   res.writeHead(301, "http://localhost:3000/");
+//   res.end();
+// };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // TODO ADD IN WHATEVER INFO SERENA NEEDS TO RENDER THE PAGE
-  res.render('index');
+  res.render('index', {
+    clientId: process.env.CLIENT_ID
+  });
 });
+
+router.get('/callback', function(req, res, next) {
+  res.send("RANDOM");
+});
+
 
 router.post('/', function(req, res, next) {
 	SC.get('/tracks', { title: req.body.title }, function(tracks) {
