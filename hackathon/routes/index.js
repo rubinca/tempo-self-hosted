9eabb13f7ae6a412a3f3007a9fe8f81489db87a2
rubinca <julierubin@Caitlin-Rubins-MacBook.local>
2016-07-09 16:02:03 -0400

    added get and post routes for index.hbs

--- hackathon/routes/index.js
+++ hackathon/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+// soundcloud authentification
+var SC = require('node-soundcloud');
+ 
+// Initialize client 
+SC.init({
+  id: process.env,
+  secret: 'universe',
+  uri: 'http://localhost:3000/'
+});
+ 
+// Connect user to authorize application 
+var initOAuth = function(req, res) {
+  var url = SC.getConnectUrl();
+ 
+  res.writeHead(301, Location: url);
+  res.end();
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // TODO ADD IN WHATEVER INFO SERENA NEEDS TO RENDER THE PAGE
+  res.render('index');
 });
+
+router.post('/', function(req, res, next) {
+	SC.get('/tracks', { title: req.body.title }, function(tracks) {
+	res.render('index', {
+		tracks: tracks
+	})
+   // $(tracks).each(function(index, track) {
+   //   $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+   // });
+  });
+})
 
 module.exports = router;
